084ce4e09ab1536eba93bad2f0198c2f6cc0e2cb
Johnny Ho <johnnyho@fas.harvard.edu>
2021-03-15 02:49:04 -0400

    rename pyroot to qpyroot

--- examples/qpyroot_interface_example.py
+++ examples/qpyroot_interface_example.py
@@ -12,13 +12,13 @@ import os, sys, inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
-import pyroot
+import qpyroot
 
 # file_path = './examples/root/muon_rtd_2020-09-08.root'
 file_path = '/Volumes/seagate/work/qpix/supernova_test/rtd/nu_e/Nu_e-rtd-39.root'
 
-file_handler = pyroot.FileHandler(file_path)
-event_handler = pyroot.EventHandler(file_handler)
+file_handler = qpyroot.FileHandler(file_path)
+event_handler = qpyroot.EventHandler(file_handler)
 
 number_entries = event_handler.number_entries()
 
